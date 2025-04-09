63e6a0d32bf7e806c48a6bf6383c0adc0b785cea
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-09 16:07:04 -0300

    add(frontend): componente App com estrutura básica e estilização inicial

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import { ProductForm } from "./components/ProductForm"
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50 p-4">
-      {" "}
       <ProductForm />
     </div>
   )

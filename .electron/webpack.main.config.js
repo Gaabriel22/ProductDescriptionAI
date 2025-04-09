4f1fbc39e7d1924114bd94558911c62fdb8b864c
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-09 16:13:05 -0300

    chore(build): melhora config webpack.main com path resolve e limpeza de output

--- .electron/webpack.main.config.js
+++ .electron/webpack.main.config.js
@@ -1,14 +1,14 @@
 const path = require("path")
 
 module.exports = {
-  entry: "./src/main/main.ts",
+  entry: path.resolve(__dirname, "../src/main/main.ts"),
   target: "electron-main",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
         exclude: /node_modules/,
+        use: "ts-loader",
       },
     ],
   },
@@ -18,5 +18,6 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "../../dist/main"),
+    clean: true, // Limpa a pasta antes de gerar novos arquivos
   },
 }

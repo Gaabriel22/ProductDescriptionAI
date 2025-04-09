b436e501935d92ea10f61655f511e32fce1e5bef
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-09 16:10:51 -0300

    refactor(types): tipagem global do window com interface ElectronAPI

--- src/renderer/types/electron-api.d.ts
+++ src/renderer/types/electron-api.d.ts
@@ -1,10 +1,14 @@
-interface Window {
-  electronAPI: {
-    fetchProductTitle: (productKey: string) => Promise<string>
-    generateDescription: (productTitle: string) => Promise<string>
-    updateProductDescription: (
-      productKey: string,
-      description: string
-    ) => Promise<void>
+export interface ElectronAPI {
+  fetchProductTitle: (productKey: string) => Promise<string>
+  generateDescription: (productTitle: string) => Promise<string>
+  updateProductDescription: (
+    productKey: string,
+    description: string
+  ) => Promise<void>
+}
+
+declare global {
+  interface Window {
+    electronAPI: ElectronAPI
   }
 }
